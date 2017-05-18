cb7f7e203a9ae60a0a670030fba9e916a9df8332
avi123123 <avilew92@gmail.com>
2017-05-18 23:50:00 +0300

    sdf

--- game_main.py
+++ game_main.py
@@ -8,8 +8,8 @@ elif argv[-1] == "neta":
     from momo_game.Neta.greenGrocerGame import GreenGrocerGame
     minigame = GreenGrocerGame
 elif argv[-1] == "avi":
-    from momo_game.Avi.coloredBoxes import coloredBoxes
-    minigame = coloredBoxes
+    from Avi.coloredBoxes import *
+    minigame = coloredGame
 else:
     from momo_game.Naama.ChaseGame import ChaseGame
     minigame = ChaseGame
