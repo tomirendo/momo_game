202051584b02b2419f8cc126a5445e0c8a720485
NetaKenneth <neta.knt@gmail.com>
2017-05-18 18:20:42 +0300

    update the change of days during the game

--- Neta/greenGrocerGame.py
+++ Neta/greenGrocerGame.py
@@ -20,6 +20,9 @@ TOMATOES = 'tomatoes'
 TOMATO_POSITION = (115+150,210+150)
 CUCUMBERS = 'cucumbers'
 CUCUMBER_POSITION = (211+150,331+150)
+TOMATO_PRICE = 0.9
+CARROT_PRICE = 1.1
+CUCUMBER_PRICE = 0.5
 
 speech_bubble_path = os.path.abspath('Neta/speechBubble.png')
 SPEECH_BUBBLE_IMAGE = pygame.image.load(speech_bubble_path)
@@ -48,10 +51,12 @@ class GreenGrocerGame():
         self.__first_shopper_position = self.__game.SCREEN_WIDTH - 220
         self.create_shoppers()
         self.__shopping_list = {TOMATOES: 0, CUCUMBERS : 0, CARROTS: 0}
+        self.__vegetables_sold = {TOMATOES: 0, CUCUMBERS : 0, CARROTS: 0}
         self.__basket_text = None
         self.__font  = pygame.font.SysFont("monospace", 17)
         self.__money = 0
         self._start_time = time.time()
+        self.__display_day_summary = False
 
 
 
@@ -92,6 +97,16 @@ class GreenGrocerGame():
         '''move the grocer according to pressed keys
         Add vegetables if needed'''
         pressed_keys = self.__game.get_keys_pressed()
+
+        if pressed_keys[pygame.K_SPACE]:
+            self.__display_day_summary = False
+            self.__money = self.display_day_sum()
+            self._start_time = time.time()
+            self.create_shoppers()
+            self.__basket = list()
+            self.__shopping_list = {TOMATOES: 0, CUCUMBERS: 0, CARROTS: 0}
+            self.__vegetables_sold = {TOMATOES: 0, CUCUMBERS: 0, CARROTS: 0}
+
         if (self.__can_move):
             if pressed_keys[pygame.K_LEFT] and self.__grocer[POSITION][0] >= 150:
                 self.__grocer[POSITION] = (self.__grocer[POSITION][0] - GreenGrocerGame.STEPSIZE, self.__grocer[POSITION][1])
@@ -102,15 +117,15 @@ class GreenGrocerGame():
                     self.start_basket()
 
 
-        if pressed_keys[pygame.K_DOWN]:
-            for vegetable in self.__stalls:
-                if (self.__grocer[POSITION][0] > self.__stalls[vegetable][POSITION][0] and self.__grocer[POSITION][0] < self.__stalls[vegetable][POSITION][1] - GreenGrocerGame.GROCER_IMAGE_WIDTH):
-                    if time.time() - self.__last_vegetable_added > 0.5 and len(self.__basket) <= 10:
-                        self.__basket.append(vegetable)
-                        self.__last_vegetable_added = time.time()
+            if pressed_keys[pygame.K_DOWN]:
+                for vegetable in self.__stalls:
+                    if (self.__grocer[POSITION][0] > self.__stalls[vegetable][POSITION][0] and self.__grocer[POSITION][0] < self.__stalls[vegetable][POSITION][1] - GreenGrocerGame.GROCER_IMAGE_WIDTH):
+                        if time.time() - self.__last_vegetable_added > 0.5 and len(self.__basket) <= 10:
+                            self.__basket.append(vegetable)
+                            self.__last_vegetable_added = time.time()
 
-            if self.__grocer[POSITION][0] >= self.__first_shopper_position - self.__grocer[WIDTH] - 30:
-                self.check_basket()
+                if self.__grocer[POSITION][0] >= self.__first_shopper_position - self.__grocer[WIDTH] - 30:
+                    self.check_basket()
 
 
     def check_basket(self):
@@ -137,8 +152,10 @@ class GreenGrocerGame():
     def get_money(self):
         '''get money for the shopping list'''
         if self.__basket:
-            self.__money += 0.9 * self.__shopping_list[TOMATOES] + 1.2 * self.__shopping_list[CARROTS] + 0.5 * self.__shopping_list[CUCUMBERS]
+            self.__money += TOMATO_PRICE * self.__shopping_list[TOMATOES] + CARROT_PRICE * self.__shopping_list[CARROTS] + CUCUMBER_PRICE * self.__shopping_list[CUCUMBERS]
             self.__shoppers.remove(self.__shoppers[0])
+            for vegetable in self.__shopping_list:
+                self.__vegetables_sold[vegetable] += self.__shopping_list[vegetable]
 
 
 
@@ -150,6 +167,8 @@ class GreenGrocerGame():
 
     def start_basket(self):
         '''start a new basket that you need to fill'''
+        if not self.__shoppers:
+            self.create_shoppers()
         self.__shopping_list = self.__shoppers[0].get_shopping_list()
         self.__basket_text = "Tomatoes: " + str(self.__shopping_list[TOMATOES]) + "     Cucumbers: " + str(self.__shopping_list[CUCUMBERS]) + "     Carrots: " + str(self.__shopping_list[CARROTS])
 
@@ -166,12 +185,22 @@ class GreenGrocerGame():
         money_text = self.__font.render("Money: " + str("%.2f" % self.__money), True, (255,255,255))
         self.__screen.blit(money_text,(10, 80))
 
-        current_time = int (time.time() - self._start_time)
-        current_time *= 4
-        hours = current_time // 60 + 9
-        minutes = current_time % 60
-        time_text = self.__font.render("Clock: " + str(hours) + ":" + str(minutes), True, (255,255,255))
-        self.__screen.blit(time_text,(10, 110))
+        if (not self.__display_day_summary):
+            current_time = int (time.time() - self._start_time)
+            current_time *= 4
+            hours = current_time // 60 + 9
+            minutes = current_time % 60
+            if (hours < 10):
+                time_text = self.__font.render("Clock: " + str(hours) + ":" + str(minutes), True, (255,255,255))
+                self.__screen.blit(time_text,(10, 110))
+            else:
+                self.__display_day_summary = True
+        else:
+            time_text = self.__font.render("Day Summary", True, (255, 255, 255))
+            self.__screen.blit(time_text, (10, 110))
+
+
+
 
 
     def draw_vegetables(self):
@@ -184,22 +213,41 @@ class GreenGrocerGame():
 
     def main_loop(self):
         '''the main loop for the minigame'''
-        self.__screen.fill((0,0,0))
-        self.__screen.blit(self.__bg_image,(150,60))
-        self.check_keys()
-        self.__screen.blit(self.__grocer[IMAGE],self.__grocer[POSITION])
-        self.draw_vegetables()
-        self.draw_shoppers()
-        self.display_basket()
+        self.__screen.fill((0, 0, 0))
+        if not (self.__display_day_summary):
+            self.__screen.blit(self.__bg_image,(150,60))
+            self.__screen.blit(self.__grocer[IMAGE],self.__grocer[POSITION])
+            self.draw_vegetables()
+            self.draw_shoppers()
+            self.display_basket()
+        else:
+            self.display_day_sum()
         self.display_sidebar()
+        self.check_keys()
         return True
+
+
+    def display_day_sum(self):
+        x_position = 150
+        y_position = 100
+        income = self.__money
+        tomato_price = self.__vegetables_sold[TOMATOES] * (TOMATO_PRICE - 0.2)
+        cucumber_price = self.__vegetables_sold[CUCUMBERS] * (CUCUMBER_PRICE - 0.2)
+        carrot_price = self.__vegetables_sold[CARROTS] * (CARROT_PRICE - 0.2)
+        text = ["Vegetables sold today:", str(self.__vegetables_sold[TOMATOES]) + " tomatoes" , str(self.__vegetables_sold[CARROTS]) + " carrots", str(self.__vegetables_sold[CUCUMBERS]) + " cucumbers" , "Expenses:" , "Tomatoes: " + str(tomato_price) , "Carrots: " + str(carrot_price) ,
+               "Cucumbers: " + str(cucumber_price) ," " , "Net Income today: " + str( "%.2f" % (income - tomato_price - cucumber_price - carrot_price)), " ", " ", " ", "Click on powerups to buy them", " ", " ", "Press space to begin the next day"]
+        for line in text:
+            text_to_print = self.__font.render(line,True,(255,255,255))
+            self.__screen.blit(text_to_print, (x_position, y_position))
+            y_position += 25
+
+        return income - tomato_price - cucumber_price - carrot_price
 
 
 
     def get_music(self):
         '''get the game's background music'''
         return None
-
 
 
     def get_loop(self):
