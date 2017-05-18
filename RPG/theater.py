4e616c878fbc8c5e76e1856e2279f9560d7c9692
Yotam Vaknin <tomirendo@gmail.com>
2017-05-19 02:23:41 +0300

    added moving boards

--- RPG/theater.py
+++ RPG/theater.py
@@ -3,3 +3,7 @@ from RPG.board import Board
 class TheaterBoard(Board):
     def find_selected_character(self):
         return self.static_characters[0]
+    def is_done(self):
+        return self.static_characters[0].is_dialog_done()
+    def next_board(self):
+        return None
